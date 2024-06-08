aded66c21e2dc112854a0a35c337f264d0f5c943
Subham90 <110160047+Subham90@users.noreply.github.com>
2024-06-08 22:36:05 +0530

    Time: 3 ms (95.01%), Space: 12.6 MB (53.25%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(int x,int y,vector<vector<int>>&grid,vector<vector<int>>&dp)
+    int solve(int x,int y,vector<vector<int>>&grid,vector<vector<int>>&dp) // Top-down Solution 
     {
        if(x==0 and y==0)
        {
@@ -25,25 +25,28 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
     int m = grid.size();
     int n = grid[0].size();
-    vector<vector<int>>dp(m+1,vector<int>(n+1,0));
-    dp[0][0] = grid[0][0];
-    for(int i=0;i<m;i++)
-    {
-       for(int j=0;j<n;j++)
-       {
-        if(i==0 and j==0)
-        {
-            continue;
+    
+    // Initialize the previous row with size n (number of columns)
+    vector<int> prev(n, 0);
+
+    for (int i = 0; i < m; ++i) {
+        vector<int> curr(n, 0);
+        for (int j = 0; j < n; ++j) {
+            if (i == 0 && j == 0) {
+                curr[j] = grid[i][j];
+            } else {
+                int up = INT_MAX, left = INT_MAX;
+                if (i > 0) {
+                    up = grid[i][j] + prev[j]; // Previous row's jth column
+                }
+                if (j > 0) {
+                    left = grid[i][j] + curr[j - 1]; // Current row's (j-1)th column
+                }
+                curr[j] = min(up, left);
+            }
         }
-        int up = INT_MAX,left = INT_MAX;
-        if(i>0)
-        up = grid[i][j] + dp[i-1][j];
-        if(j>0)
-        left = grid[i][j] + dp[i][j-1];
-           
-         dp[i][j] = min(up,left);
-       }
+        prev = curr;
     }
-    return dp[m-1][n-1];
-    }
+    return prev[n - 1]; // Last element in the last row
+}
 };
