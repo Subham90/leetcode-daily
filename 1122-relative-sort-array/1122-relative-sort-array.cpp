33f20885b1350d4d06fe437670f970cc0d3e6217
Subham90 <110160047+Subham90@users.noreply.github.com>
2024-06-11 12:51:07 +0530

    Time: 3 ms (53.89%), Space: 10.2 MB (41.93%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        unordered_map<int, int> countMap;
-        
-        // Count occurrences of each number in arr1
+        map<int, int> countMap;
         for (const int& num : arr1) {
             countMap[num]++;
         }
-        
         vector<int> ans;
-        
-        // Add elements of arr2 in the order they appear in arr2
         for (const int& num : arr2) {
             while (countMap[num] > 0) {
                 ans.push_back(num);
                 countMap[num]--;
             }
         }
-        
-        // Add remaining elements not in arr2, in ascending order
-        vector<int> remaining;
         for (const auto& pair : countMap) {
             while (pair.second > 0) {
-                remaining.push_back(pair.first);
+                ans.push_back(pair.first);
                 countMap[pair.first]--;
             }
         }
-        sort(remaining.begin(), remaining.end());
-        ans.insert(ans.end(), remaining.begin(), remaining.end());
-        
         return ans;
     }
 };
