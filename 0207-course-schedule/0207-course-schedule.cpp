11b1c230b3f42c72e602eb77382aef7418b59048
Subham90 <110160047+Subham90@users.noreply.github.com>
2024-06-26 23:54:34 +0530

    Time: 19 ms (37.08%), Space: 17.3 MB (61.97%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        vector<int>adj[n];
+        int size = prerequisites.size();
+        for(int i=0;i<size;i++)
+        {
+            auto it = prerequisites[i];
+            adj[it[0]].push_back(it[1]);
+        }
+        
+        vector<int>indegree(n,0);
+        for(int i=0;i<n;i++)
+        {
+            for(auto it: adj[i])
+            {
+                indegree[it]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.push(i);
+            }
+        }
+        
+        int cnt = 0;
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            cnt++;
+            for(auto it: adj[node])
+            {
+                indegree[it]--;
+                if(indegree[it]==0)
+                {
+                    q.push(it);
+                }
+            }
+        }
+        if(cnt == n)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
