42530a207ead1715b73cd74fb83bd33c4a50930a
Subham Ghosh <110160047+Subham90@users.noreply.github.com>
2024-07-25 12:01:07 +0530

    Time: 111 ms (66.08%), Space: 84.4 MB (37.41%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -2,53 +2,39 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
-        vector<int>prev(n);
-        vector<int>next(n);
+        vector<int>prev(n,-1);
+        vector<int>next(n,n);
         stack<int>st;
-        
-        for(int i=0;i<n;i++)
-        {
-            while(!st.empty() and heights[st.top()] >= heights[i])
-            {
-                st.pop();
-            }
-            if(st.empty())
-            {
-                prev[i] = -1;
-            }
-            else
-            {
-                prev[i] = st.top();
-            }
-            
-            st.push(i);
-        }
-        
-        st=stack<int>();
         for(int i=n-1;i>=0;i--)
         {
-            while(!st.empty() and heights[st.top()] >= heights[i])
+            while(!st.empty() and heights[i] <= heights[st.top()])
             {
                 st.pop();
             }
-            if(st.empty())
-            {
-                next[i] = n;
-            }
-            else
+            if(!st.empty())
             {
                 next[i] = st.top();
             }
-            
             st.push(i);
         }
-        
-        int ans = INT_MIN;
+        stack<int>().swap(st);
         for(int i=0;i<n;i++)
         {
-            int mx = (next[i] - prev[i] - 1)*heights[i];
-            ans = max(ans,mx);
+            while(!st.empty() and heights[i] <= heights[st.top()])
+            {
+                st.pop();
+            }
+            if(!st.empty())
+            {
+                prev[i] = st.top();
+            }
+            st.push(i);
         }
+    int ans=INT_MIN;
+    for(int i=0;i<n;i++)
+    {
+        ans = max(ans,(next[i]-prev[i]-1)*heights[i]);
+    }
         return ans;
     }
 };
