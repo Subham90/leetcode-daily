413b2cc306f4bb69599d63973299dc3f83173352
Subham Ghosh <110160047+Subham90@users.noreply.github.com>
2024-07-25 12:32:51 +0530

    Time: 31 ms (60.69%), Space: 20.7 MB (30.43%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,72 +1,62 @@
 class Solution {
-public:
-    int largestRectangleArea(vector<int>&heights) {
+    private:
+     int largestRectangleArea(vector<int >& heights) {
         int n = heights.size();
-        vector<int>prev(n);
-        vector<int>next(n);
+        vector<int>prev(n,-1);
+        vector<int>next(n,n);
         stack<int>st;
-        
-        for(int i=0;i<n;i++)
-        {
-            while(!st.empty() and heights[st.top()] >= heights[i])
-            {
-                st.pop();
-            }
-            if(st.empty())
-            {
-                prev[i] = -1;
-            }
-            else
-            {
-                prev[i] = st.top();
-            }
-            
-            st.push(i);
-        }
-        
-        st=stack<int>();
         for(int i=n-1;i>=0;i--)
         {
-            while(!st.empty() and heights[st.top()] >= heights[i])
+            while(!st.empty() and heights[i] <= heights[st.top()])
             {
                 st.pop();
             }
-            if(st.empty())
-            {
-                next[i] = n;
-            }
-            else
+            if(!st.empty())
             {
                 next[i] = st.top();
             }
-            
             st.push(i);
         }
-        
-        int ans = INT_MIN;
+        stack<int>().swap(st);
         for(int i=0;i<n;i++)
         {
-            int mx = (next[i] - prev[i] - 1)*heights[i];
-            ans = max(ans,mx);
+            while(!st.empty() and heights[i] <= heights[st.top()])
+            {
+                st.pop();
+            }
+            if(!st.empty())
+            {
+                prev[i] = st.top();
+            }
+            st.push(i);
         }
+    int ans=INT_MIN;
+    for(int i=0;i<n;i++)
+    {
+        ans = max(ans,(next[i]-prev[i]-1)*(heights[i]));
+    }
         return ans;
     }
+public:
     int maximalRectangle(vector<vector<char>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
         vector<int>dp(m,0);
-        int ans = 0;
+        int ans=INT_MIN;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                    if(matrix[i][j]=='0')
-                    dp[j] = 0;
-                    else
+                if(matrix[i][j]=='0')
+                {
+                    dp[j]=0;
+                }
+                else
+                {
                     dp[j]++;
+                }
             }
-            int p = largestRectangleArea(dp);
-            ans = max(ans,p);
+            ans = max(ans,largestRectangleArea(dp));
         }
         return ans;
     }
